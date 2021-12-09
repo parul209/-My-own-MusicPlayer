d72a5ddbd0dff0e3a33bcb7257bd7b56e87c880d
Paro <paruliiitb2019@gmail.com>
2021-12-09 15:51:16 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,70 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Baari",
+    artist: "Bilal Saeed",
+    img: "music7",
+    src: "music7"
+  },
+    {
+      name: "Raabta- AgentVinod",
+      artist: "Shreya Ghoshal",
+      img: "music1",
+      src: "music1"
+    },
+    {
+      name: "Guitar",
+      artist: "Album",
+      img: "guitar",
+      src: "guitar"
+    },
+    {
+      name: "Jannat",
+      artist: "B Praak",
+      img: "music2",
+      src: "music2"
+    },
+    {
+      name: "Raatan Lambiyan- Shershah",
+      artist: "Jubin Nautiyal",
+      img: "music3",
+      src: "music3"
+    },
+    {
+      name: "Dooriyan- Love AAJ Kal",
+      artist: "Mohit Chauhan",
+      img: "music4",
+      src: "music4"
+    },
+    {
+      name: "Ajj din chadheya - Love Aj Kal",
+      artist: "Rahat Fateh Ali Khan",
+      img: "music5",
+      src: "music5"
+    },
+    {
+      name: "SHUBHANALLAH- YJHD",
+      artist: "Shilpa Rao",
+      img: "music6",
+      src: "music6"
+    },
+    
+
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
